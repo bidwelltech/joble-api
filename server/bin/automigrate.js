5c70779e7b83e147eae62629af451f96ae62c67c
cashweaver <cashbweaver@gmail.com>
2016-11-03 23:02:30 -0700

    Update automigrate

--- server/bin/automigrate.js
+++ server/bin/automigrate.js
@@ -9,7 +9,11 @@ const defaultModels = [
   'RoleMapping',
   'Role',
 ];
-const appModelKeys = Object.keys(app.models).filter(key => !defaultModels.includes(key));
+const appModelKeys = Object.keys(app.models)
+  .filter(key => !defaultModels.includes(key))
+  .map(key => key.toLowerCase())
+  // ref: http://stackoverflow.com/a/14438954
+  .filter((v, i, a) => a.indexOf(v) === i);
 //console.log(psql.settings);
 
 appModelKeys.forEach((key) => {
