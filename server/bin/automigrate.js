af0728c4eba3538badfffb2e1cfa8d2a91910fd4
cashweaver <cashbweaver@gmail.com>
2016-11-06 22:11:12 -0800

    Fix lint error

--- server/bin/automigrate.js
+++ server/bin/automigrate.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-const app = require(path.resolve(__dirname, '../server'))
+const app = require('../server');
 
 const psql = app.datasources.apiDb;
 
