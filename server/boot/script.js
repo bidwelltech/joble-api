add26553946d6b09e1b69f1a155ceca0625fae70
cashweaver <cashbweaver@gmail.com>
2016-11-14 23:23:07 -0800

    Create two test users on boot

--- server/boot/script.js
+++ server/boot/script.js
@@ -2,90 +2,47 @@ const Promise = require('bluebird');
 
 module.exports = (app) => {
   if (process.env.NODE_ENV === 'test') {
-    const RoleMapping = app.models.RoleMapping;
-    const Role = app.models.Role;
     const User = app.models.user;
 
-    let testUser;
-    let testAdmin;
+    console.log('creating users');
+
 
     // Use bluebird Promises to create a promise chain as
     // Loopback doesn't support promises yet, and promisify(All) doesn't work
     // https://github.com/strongloop/loopback/issues/418#issue-38984704
-    return new Promise((resolve, reject) => {
-      // Create testuser
-      User.find({ where: { username: 'testuser' } }, (findUserError, users) => {
+    new Promise((resolve, reject) => {
+      // Create testuser1
+      User.find({ where: { username: 'testuser1' } }, (findUserError, users) => {
         if (findUserError) return reject(findUserError);
 
         if (users.length === 0) {
           User.create({
-            username: 'testuser',
-            email: 'testuser@example.com',
-            password: 'testuser',
-          }, (createUserError, user) => {
+            username: 'testuser1',
+            email: 'testuser1@example.com',
+            password: 'testuser1',
+          }, (createUserError) => {
             if (createUserError) return reject(createUserError);
-
-            testUser = user;
             return resolve();
           });
         }
-
-        testUser = users[0];
         return resolve();
       });
     })
     .then(() => (
       new Promise((resolve, reject) => {
-        // Create testadmin
-        User.find({ where: { username: 'testadmin' } }, (findUserError, users) => {
-          if (findUserError) {
-            console.log(findUserError);
-            return reject('cats');
-          }
+        User.find({ where: { username: 'testuser2' } }, (findUserError, users) => {
+          if (findUserError) return reject(findUserError);
 
           if (users.length === 0) {
             User.create({
-              username: 'testadmin',
-              email: 'testadmin@example.com',
-              password: 'testadmin',
-            }, (createUserError, user) => {
-              if (createUserError) return reject('dogs');
-
-              testAdmin = user;
+              username: 'testuser2',
+              email: 'testuser2@example.com',
+              password: 'testuser2',
+            }, (createUserError) => {
+              if (createUserError) return reject(createUserError);
               return resolve();
             });
           }
-
-          testAdmin = users[0];
-          return resolve();
-        });
-      })
-    ))
-    .then(() => (
-      new Promise((resolve, reject) => {
-        Role.find({ where: { name: 'admin' } }, (findRoleError, roles) => {
-          if (findRoleError) {
-            console.log(findRoleError);
-            return reject('cats');
-          }
-
-          if (roles.length === 0) {
-            Role.create({ name: 'admin' }, (err, role) => {
-              if (err) return reject(err);
-              console.log('Created role admin');
-
-              return role.principals.create({
-                principalType: RoleMapping.USER,
-                principalId: testAdmin.id,
-              }, (principalCreationErr) => {
-                if (principalCreationErr) return reject(principalCreationErr);
-
-                console.log(`Set ${testAdmin.username} as an admin`);
-                return resolve();
-              });
-            });
-          }
-
           return resolve();
         });
       })
